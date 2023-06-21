c5ade903c1687a711ec8220c27b4b0ebbb8e730d
OfentseLearnsCode <malebyeoc@gmail.com>
2023-06-21 15:53:26 +0200

    State management

---
+++ SwiftCheckout/UpdatePasswordViewController.swift
@@ -0,0 +1,50 @@
+////
+////  UpdatePasswordViewController.swift
+////  SwiftCheckout
+////
+////  Created by DA MAC M1 137 on 2023/06/21.
+////
+//
+////
+////  UpdatePasswordVC.swift
+////  SwiftCheckout
+////
+////  Created by DA MAC M1 137 on 2023/06/21.
+////
+//
+//import UIKit
+//
+//class UpdatePasswordVC: UIViewController {
+//
+//    @IBOutlet weak var CurrentPasswordTextField: UITextField!
+//    @IBOutlet weak var newPasswordTextField: UITextField!
+//    @IBOutlet weak var confirmPasswordTextField: UITextField!
+//    @IBOutlet weak var updateButton: UIButton!
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//    }
+//
+//  // @objc func updateButton (addTarget (self, action: #selector(updateButtonTapped), for:.touchUpInside) {
+//
+//        @objc func updateButtonTapped() {
+//
+//            guard let currentPassword = CurrentPasswordTextField.text, !currentPassword.isEmpty else {
+////                password
+//                return
+//            }
+//
+//            guard let newPassword = newPasswordTextField.text, !newPassword.isEmpty else {
+//                return
+//            }
+//            guard let confirmPassword = confirmPasswordTextField.text, !confirmPassword.isEmpty else {
+//                return
+//            }
+//            if newPassword != confirmPassword {
+//                return
+//
+//            }
+//        }
+//    }
